928cc788b2d4274670c91775db3ae642aa3ed205
aerotone <mistercostello@msn.com>
2017-08-26 10:48:19 -0700

    Added React Bootstrap and React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ReactBootstrap.Button bsStyle="primary" bsSize="small">
+          Something
+        </ReactBootstrap.Button>
       </div>
     );
   }
